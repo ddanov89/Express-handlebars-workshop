261171e89306e8168fdfb303f635b24bb5ff2309
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-05-27 21:55:37 +0300

    Catalog, create, details functionalities added

---
+++ src/controllers/catalog.js
@@ -0,0 +1,21 @@
+const { getAllMovies, getMovieById } = require("../services/movie");
+
+module.exports = {
+    home: async (req, res) => {
+        const movies = await getAllMovies();
+        res.render('home', { movies });
+    },
+    details: async (req, res) => {
+        const { id } = req.params;
+        const movie = await getMovieById(id);
+        if (!movie) {
+            res.render('404');
+            return;
+        }
+        movie.startRating = "&#x2605;".repeat(movie.rating);
+        res.render('details', { movie });
+    },
+    search: (req, res) => {
+        res.render('search');
+    }
+};
